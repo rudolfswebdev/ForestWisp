29a536e5360134ab1924d54a41385131484b690b
daveanders <44576388+daveanders@users.noreply.github.com>
2020-06-26 21:45:45 +0300

    Player moves, camera follows

---
+++ Assets/Scripts/Global/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+   }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 3.0f;
+
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
