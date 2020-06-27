33a6ef8377e764c27246853da3601147cac744f5
Rudolfs Balcers <rudolfswebdev@gmail.com>
2020-06-27 23:51:46 +0300

    Added all minions to the game, added logic to count minions

---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    public int minions = 0;
+    public int orbs = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+     //Use the same vars you use to draw your Overlap SPhere to draw your Wire Sphere.
+     Gizmos.DrawWireSphere(transform.position, 10.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 20.0f);
+        
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            switch (hitColliders[i].gameObject.tag)
+            {
+                case "minion":
+                    Debug.Log("THERE IS A MINION");
+                    if (Input.GetKeyDown(KeyCode.E) && hitColliders[i].gameObject.GetComponent<FollowPlayer>().stationary == true)
+                    {
+              
+                        hitColliders[i].gameObject.GetComponent<FollowPlayer>().StartFollow();
+                    }
+                    break;
+            }
+            i++;
+        }
+    }
+
+    public void RemoveOrbs(int toll)
+    {
+        orbs -= toll;
+    }
+
+}
--- Assets/Scripts/Minions/FollowPlayer.cs
+++ Assets/Scripts/Minions/FollowPlayer.cs
@@ -5,14 +5,37 @@ public class FollowPlayer : MonoBehaviour
 {
     public Transform transformToFollow;
     NavMeshAgent agent;
+    public bool stationary = true;
+    private Animator anim;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         //Follow the player
-        agent.SetDestination(transformToFollow.transform.position);
+        if(stationary == false)
+        {
+            anim.SetBool("isWalking", true);
+            agent.SetDestination(transformToFollow.transform.position);
+        }
+
+        if(agent.velocity == new Vector3(0, 0, 0))
+        {
+            anim.SetBool("isWalking", false);
+        }else
+        {
+            anim.SetBool("isWalking", true);
+        }
+
+        
+    }
+
+    public void StartFollow()
+    {
+        stationary = false;
     }
 }
