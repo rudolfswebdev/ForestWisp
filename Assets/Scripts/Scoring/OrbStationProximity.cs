9f6886c46e74acb991cfa9d8267954fd121bc825
daveanders <44576388+daveanders@users.noreply.github.com>
2020-06-27 18:34:02 +0300

    Initial commit

---
+++ Assets/Scripts/Scoring/OrbStationProximity.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class OrbStationProximity : MonoBehaviour
+{
+    float detectionRange = 2;
+    public float thrust = 1.0f;
+    bool closeEnough;
+    public Transform player;
+ 
+ void Update()
+    {
+        closeEnough = false;
+        if (Vector3.Distance(player.position, transform.position) <= detectionRange)
+        {
+            closeEnough = true;
+        }
+        if (closeEnough && Input.GetKeyUp(KeyCode.E))
+        {
+            // Orb falls
+            gameObject.GetComponentInChildren<Rigidbody>().AddForce(transform.forward * thrust);
+        }
+    }
+ }
+
