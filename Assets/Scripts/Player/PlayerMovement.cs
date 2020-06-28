95ca14893dde8edb4df950acbf088119161da542
Rudolfs Balcers <rudolfswebdev@gmail.com>
2020-06-28 10:59:23 +0300

    Fixed character controller gravity

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -27,7 +27,8 @@ public class PlayerMovement : MonoBehaviour
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = UnityEngine.Quaternion.Euler(0f, angle, 0f);
             UnityEngine.Vector3 moveDir = UnityEngine.Quaternion.Euler(0f, targetAngle, 0f) * UnityEngine.Vector3.forward;
-            controller.SimpleMove(moveDir.normalized * speed * Time.deltaTime);
+            moveDir.y = -9.8f * Time.deltaTime;
+            controller.Move(moveDir * speed * Time.deltaTime);
 
             animator.SetBool("isWalking", true);
 
